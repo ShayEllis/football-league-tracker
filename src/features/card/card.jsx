299602fc47a4450692824050aab7f4c8d7aa4a36
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-09-08 21:43:30 -0700

    adjusted card table and table drop-down

--- src/features/card/card.jsx
+++ src/features/card/card.jsx
@@ -5,6 +5,8 @@ import footballStadium from '../../assets/football-stadium.jpg'
 const Card = ({
   cardData: { leagueName, teamName, ranking, buyIn, payout, platform },
 }) => {
+  const tableCollapseId = `collapse${leagueName.split(' ').join('-')}`
+
   return (
     <div className='card small'>
       <div className='card-header'>
@@ -12,10 +14,10 @@ const Card = ({
         <p className='card-subtitle'>{teamName}</p>
       </div>
       {/* <img src={footballStadium} /> */}
-      <div className='card-body'>
-        <p className='card-text'>10 - Team</p>
+      <div className='card-body pb-0'>
+        <p className='card-text text-center'>10 - Team</p>
         <div className='table-responsive'>
-          <table className='table table-striped table-sm table-hover'>
+          <table className='table table-striped table-sm table-hover table-borderless'>
             <tbody>
               <tr>
                 <th scope='row'>Platform:</th>
@@ -24,10 +26,6 @@ const Card = ({
               <tr>
                 <th scope='row'>Buy-In:</th>
                 <td>{buyIn}</td>
-              </tr>
-              <tr>
-                <th scope='row'>Payout:</th>
-                <td>{payout}</td>
               </tr>
               <tr>
                 <th scope='row'>Draft Rank:</th>
@@ -40,6 +38,39 @@ const Card = ({
               <tr>
                 <th scope='row'>Playoff Teams:</th>
                 <td>{ranking}</td>
+              </tr>
+              <tr
+                className=''
+                data-bs-toggle='collapse'
+                data-bs-target={`#${tableCollapseId}`}
+                aria-expanded='false'
+                aria-controls={tableCollapseId}>
+                <th scope='row'>Payout 1:</th>
+                <td className='dropdown-toggle'>{payout}</td>
+              </tr>
+              <tr>
+                <th className='p-0' scope='row'>
+                  <div className='collapse lh-lg ps-1' id={tableCollapseId}>
+                    Payout 2:
+                  </div>
+                </th>
+                <td className='p-0'>
+                  <div className='collapse lh-lg ps-1' id={tableCollapseId}>
+                    {payout}
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <th className='p-0' scope='row'>
+                  <div className='collapse lh-lg ps-1' id={tableCollapseId}>
+                    Payout 3:
+                  </div>
+                </th>
+                <td className='p-0'>
+                  <div className='collapse lh-lg ps-1' id={tableCollapseId}>
+                    {payout}
+                  </div>
+                </td>
               </tr>
             </tbody>
           </table>
