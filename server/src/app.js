3e8f573f214c8fb6d3fc3cd6bfd720623f304618
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-09-13 20:25:08 -0700

    Created first route and setup logging and parsing

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,32 @@
 import express from 'express'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 // Import routes
-import leagueRouter from './routes/league'
-
-// Import environment variables
+import leagueRouter from './routes/leagueRouter.js'
+// Setup environment variables
 dotenv.config()
-
+// Initialize the express app
 const app = express()
 const PORT = process.env.PORT || 4000
+// Initialize morgan for logging
+app.use(morgan('short'))
+// Use experess.json() to parse the incomming HTTP request body - available under the req.body property
+app.use(express.json())
+// Use express.urlencoded() to parse incomming requests with url encoded payloads - available under the req.body property
+app.use(express.urlencoded({ extended: true }))
+// Initialize cors
+const corsOptions = {
+  origin: '*', // Only the origins listed here will be allowed
+  methods: ['get', 'put', 'post', 'delete'],
+  credentials: true,
+  maxAge: 1440, // One day in seconds
+  optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+// Initialize cookie-parser to parse the cookie header, available under req.cookies
+app.use(cookieParser())
 
 app.use('/league', leagueRouter)
 
---
+++ server/src/routes/leagueRouter.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const leagueRouter = express.Router()
+
+leagueRouter.get('/', (req, res, next) => {
+  if (true) {
+    res
+      .status(200)
+      .json({ message: 'Successful response from the league Route!' })
+  } else {
+    res.status(404).send()
+    //res.sendStatus(404)
+  }
+})
+
+export default leagueRouter
