a84bfba0a7d4f937dcde51450f980235858edc96
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-09-29 17:23:54 -0700

    continued working on updateLeage route and query

--- server/src/controllers/leaguesControllers.js
+++ server/src/controllers/leaguesControllers.js
@@ -7,7 +7,8 @@ export const getLeagues = async (req, res) => {
     const data = await leagueModel.select(
       'id, league_name AS "leagueName", team_name AS "teamName", team_count as "teamCount", platform, draft_date as "draftDate", buy_in as "buyIn", draft_rank as "draftRank", team_rank AS "teamRank", playoff_teams AS "playoffTeams", payout_1 AS payout1, payout_2 AS payout2, payout_3 AS payout3'
     );
-    res.status(200).json({ leagues: data.rows });
+    const filteredData = Model.filterQueryResponse(data.rows);
+    res.status(200).json({ leagues: filteredData });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -28,7 +29,8 @@ export const addLeague = async (req, res) => {
 
   try {
     const data = await leagueModel.insertWithReturn(columns, values);
-    res.status(200).json({ leagues: data.rows });
+    const filteredData = Model.filterQueryResponse(data.rows);
+    res.status(200).json({ leagues: filteredData });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -42,17 +44,23 @@ export const updateLeague = async (req, res) => {
       return `_${match.charAt(0).toLowerCase() + match.slice(1)}`;
     })
   );
-  // Create values string - `'${league_name}', '${team_name}', '${team_count}', '${platform}', '${draft_date}', '${buy_in}', '${draft_rank}', '${team_rank}', '${playoff_teams}', '${payout_1}', '${payout_2}', '${payout_3}'`
-  const values = Object.values(req.body)
-    .map((val) => `'${val}'`)
-    .join(', ');
+  // Create values array - ['${league_name}', '${team_name}', '${team_count}', '${platform}', '${draft_date}', '${buy_in}', '${draft_rank}', '${team_rank}', '${playoff_teams}', '${payout_1}', '${payout_2}', '${payout_3}']
+  const values = Object.values(req.body).map((val) => `'${val}'`);
 
-  console.log(columns)
-  console.log(values)
+  // Create newValues string from query - 'id = '275', league_name = '11', team_name = '2', team_count = '2', platform = '2', buy_in = '200''
+  const newValues = columns.reduce((pre, cur, idx) => {
+    if (cur !== 'id') {
+      let stringVal = pre.concat(`${cur} = ${values[idx]}`);
+      if (idx < values.length - 1) stringVal += ', ';
+      return stringVal;
+    }
+    return pre;
+  }, '');
 
   try {
-    // const data = await leagueModel.insertWithReturn(columns, values);
-    // res.status(200).json({ leagues: data.rows });
+    const data = await leagueModel.updateWithReturn(req.body.id, newValues);
+    const filteredData = Model.filterQueryResponse(data.rows);
+    res.status(200).json({ leagues: filteredData });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -61,7 +69,8 @@ export const updateLeague = async (req, res) => {
 export const deleteLeague = async (req, res) => {
   try {
     const data = await leagueModel.removeByIdWithReturn(req.params.id);
-    res.status(200).json({ league: data.rows });
+    const filteredData = Model.filterQueryResponse(data.rows);
+    res.status(200).json({ leagues: filteredData });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
--- server/src/models/model.js
+++ server/src/models/model.js
@@ -10,6 +10,15 @@ class Model {
     );
   }
 
+  static filterQueryResponse(responseValue) {
+    return responseValue.map((row) => {
+      return Object.keys(row).reduce((obj, key) => {
+        const val = row[key] || '';
+        return { ...obj, [key]: val };
+      }, {});
+    });
+  }
+
   async select(columns, clause) {
     let query = `
       SELECT ${columns} 
@@ -25,24 +34,26 @@ class Model {
       VALUES (${values})
       RETURNING id, league_name AS "leagueName", team_name AS "teamName", team_count as "teamCount", platform, draft_date as "draftDate", buy_in as "buyIn", draft_rank as "draftRank", team_rank AS "teamRank", playoff_teams AS "playoffTeams", payout_1 AS payout1, payout_2 AS payout2, payout_3 AS payout3
     `;
+
     return this.pool.query(query);
   }
 
-  async updateWithReturn(columns) {
+  async updateWithReturn(id, newValues) {
     const query = `
       UPDATE ${this.table}
       SET 
-        ''
+        ${newValues}
       WHERE id = ${id}
-      RETURNING *
+      RETURNING id, league_name AS "leagueName", team_name AS "teamName", team_count as "teamCount", platform, draft_date as "draftDate", buy_in as "buyIn", draft_rank as "draftRank", team_rank AS "teamRank", playoff_teams AS "playoffTeams", payout_1 AS payout1, payout_2 AS payout2, payout_3 AS payout3
     `;
+    return this.pool.query(query);
   }
 
   async removeByIdWithReturn(id) {
     const query = `
       DELETE FROM ${this.table}
       WHERE id = ${id} 
-      RETURNING *
+      RETURNING id, league_name AS "leagueName", team_name AS "teamName", team_count as "teamCount", platform, draft_date as "draftDate", buy_in as "buyIn", draft_rank as "draftRank", team_rank AS "teamRank", playoff_teams AS "playoffTeams", payout_1 AS payout1, payout_2 AS payout2, payout_3 AS payout3
     `;
     const data = await this.pool.query(query);
     if (data.rows.length > 0) {
